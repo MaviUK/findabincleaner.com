1a287812445c750ecd50557a5f71492691ad76b1
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-10-04 20:57:11 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css"; // ok to keep/remove
 import 'leaflet/dist/leaflet.css';
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
