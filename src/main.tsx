842e81415806711076bcb2edc1b5c07924b80fb5
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-10-05 23:57:19 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // ok to keep/remove
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
+
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
