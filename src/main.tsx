8329b4ebe23400dda7171c85ed8c1a490daf83f6
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-10-06 00:46:48 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // ok to keep/remove
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-draw/dist/leaflet.draw.css';
+import 'leaflet-draw';                        // registers draw controls
+import 'leaflet-draw/dist/leaflet.draw.css';  // styles for draw UI
+
 
 
 
