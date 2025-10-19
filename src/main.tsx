76fe5b2a002cb91e0072d9bd4a3a5a02482bda19
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-10-19 08:48:57 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // ok to keep/remove
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-draw';                        // registers draw controls
-import 'leaflet-draw/dist/leaflet.draw.css';  // styles for draw UI
-
-
-
+import "./index.css"; // keep your global styles
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
